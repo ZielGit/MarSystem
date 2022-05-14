490930e1e60304c76b0a22d2ed8030c9e85d7a0b
Ziel <frans_421_12@hotmail.com>
2022-05-14 00:00:07 -0500

    User seeder, se quito registro y protegio vistas

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Frans',
+            'email' => 'frans@gmail.com',
+            'password' => bcrypt('SistemaMar')
+        ]);
+
+        User::create([
+            'name' => 'Roger',
+            'email' => 'J09451k@upla.edu.pe',
+            'password' => bcrypt('chupaquino')
+        ]);
+
+        User::create([
+            'name' => 'Jessica',
+            'email' => 'jessica-991@outlook.es',
+            'password' => bcrypt('administradora')
+        ]);
+    }
+}
