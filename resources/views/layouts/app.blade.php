f057d8c2d228668fa2be0375c926be65daf5aa52
Ziel <frans_421_12@hotmail.com>
2022-05-14 00:35:56 -0500

    feat: template sneat sidebar y navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,19 +7,44 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('sneat/assets/img/favicon/favicon.ico') }}" />
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        @livewireStyles
+        <!-- Icons. Uncomment required icon fonts -->
+        <link rel="stylesheet" href="{{ asset('sneat/assets/vendor/fonts/boxicons.css') }}" />
+
+        <!-- Core CSS -->
+        <link rel="stylesheet" href="{{ asset('sneat/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
+        <link rel="stylesheet" href="{{ asset('sneat/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
+        <link rel="stylesheet" href="{{ asset('sneat/assets/css/demo.css') }}" />
+
+        <!-- Vendors CSS -->
+        <link rel="stylesheet" href="{{ asset('sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
+        <link rel="stylesheet" href="{{ asset('sneat/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+
+        <!-- Page CSS -->
+
+        <!-- Helpers -->
+        <script src="{{ asset('sneat/assets/vendor/js/helpers.js') }}"></script>
+
+        <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+        <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+        <script src="{{ asset('sneat/assets/js/config.js') }}"></script>
+
+        {{-- @livewireStyles --}}
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <x-jet-banner />
+        {{-- <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
@@ -37,10 +62,50 @@
             <main>
                 {{ $slot }}
             </main>
+        </div> --}}
+
+        <!-- Layout wrapper -->
+        <div class="layout-wrapper layout-content-navbar">
+            <div class="layout-container">
+                <!-- Menu -->
+                @include('layouts.sidebar')
+                <!-- / Menu -->
+
+                <!-- Layout container -->
+                <div class="layout-page">
+                    <!-- Navbar -->
+                    @include('layouts.navbar')
+                    <!-- / Navbar -->
+                </div>
+            </div>
         </div>
 
         @stack('modals')
 
-        @livewireScripts
+        {{-- @livewireScripts --}}
+
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="{{ asset('sneat/assets/vendor/libs/jquery/jquery.js') }}"></script>
+        <script src="{{ asset('sneat/assets/vendor/libs/popper/popper.js') }}"></script>
+        <script src="{{ asset('sneat/assets/vendor/js/bootstrap.js') }}"></script>
+        <script src="{{ asset('sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+        <script src="{{ asset('sneat/assets/vendor/js/menu.js') }}"></script>
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+        <script src="{{ asset('sneat/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+
+        <!-- Main JS -->
+        <script src="{{ asset('sneat/assets/js/main.js') }}"></script>
+
+        <!-- Page JS -->
+        <script src="{{ asset('sneat/assets/js/dashboards-analytics.js') }}"></script>
+
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+        @stack('scripts')
     </body>
 </html>
