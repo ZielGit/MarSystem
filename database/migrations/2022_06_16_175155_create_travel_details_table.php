41be6616f68156b4df7cdd98ae929fd6206bfa2f
Ziel <frans_421_12@hotmail.com>
2022-07-11 22:51:05 -0500

    feat: precio por kilo

--- database/migrations/2022_06_16_175155_create_travel_details_table.php
+++ database/migrations/2022_06_16_175155_create_travel_details_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained();
             $table->foreignId('product_type_id')->constrained();
             $table->integer('weight');
+            $table->decimal('price_kilo');
             $table->decimal('total_price');
             $table->timestamps();
         });
