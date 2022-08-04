ce87938c31b727dfb610edd7abec345ba7d80f7a
Ziel <frans_421_12@hotmail.com>
2022-08-03 20:43:38 -0500

    show travel

---
+++ resources/views/admin/travel/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title') {{ __('Travel Details') }} @endsection
+@section('content')
+<div class="container-xxl flex-grow-1 container-p-y">
+    
+</div>
+@endsection
